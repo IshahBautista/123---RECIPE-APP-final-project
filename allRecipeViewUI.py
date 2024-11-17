5d33c5f13c4a820e3f564bc3ead6877dd2e74f95
IshahBautista <ilbautista@up.edu.ph>
2024-11-17 23:25:48 +0800

    allRecipeViewUI.py

--- allRecipeViewUI.py
+++ allRecipeViewUI.py
@@ -1,6 +1,22 @@
 import flet as ft
 from flet import *
 from searchBarUI import CustomSearchBar
+from recipeCard import RecipeCard
+
+
+# NEED TO CONNECT THE SEARCH BAR TO THE DISPLAY RECIPE SOMEHOW? 
+
+class DisplayRecipesfromSearch:
+    def __init__(self, container):
+        self.__container = container
+
+    def updateRecipeCards(self, recipeList):
+        self.__container.controls.clear()  # Clear old recipes
+        for recipe in recipeList:
+            newCard = RecipeCard(recipe)
+            self.__container.controls.append(newCard.getCardView())
+        self.__container.update()  # Refresh the UI
+
 
 def main(page: ft.Page):
     page.title = "All Recipes View"
@@ -9,6 +25,16 @@ def main(page: ft.Page):
     page.window.width = 440
     page.window.height = 956
     page.window.resizable = False
+
+    RecipeContainer = ft.Container(
+        width=400,
+        height=600,
+        padding=10,
+        margin=5,
+        border_radius=20,
+        bgcolor=ft.colors.GREY_200,
+        content=ft.Column(scroll=ft.ScrollMode.ALWAYS)
+    )
 
     recipeSearchBar = CustomSearchBar()
 
@@ -21,14 +47,6 @@ def main(page: ft.Page):
         bgcolor=ft.colors.GREY_200,
     )
 
-    RecipeContainer = ft.Container(
-        width=400,
-        height=600,
-        padding=10,
-        margin=5,
-        border_radius=20,
-        bgcolor=ft.colors.GREY_200,
-    )
 
     page.add(
         ft.Column(
